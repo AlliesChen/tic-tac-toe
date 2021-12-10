2543b05a36abd2eb5f5cd118057d7037d9e03501
AlliesChen <cyphuaenng@pm.me>
2021-12-10 11:26:48 +0800

    circle-sign toggle tested on step 4/7

--- js-file.js
+++ js-file.js
@@ -1,12 +1,36 @@
 // create a two dimentional array
-const gameBoard = (function createArray(length) {
-    const arr = new Array(length || 0);
-    let i = length;
-
-    if (arguments.length > 1) {
-        const args = Array.prototype.slice.call(arguments, 1);
-        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+const gameBoard = (() => {
+    function _createArray(length) {
+        const arr = new Array(length || 0);
+        let i = length;
+    
+        if (arguments.length > 1) {
+            const args = Array.prototype.slice.call(arguments, 1);
+            while(i--) arr[length-1 - i] = _createArray.apply(this, args);
+        }
+        return arr;
     }
 
-    return arr;
-})(3, 3);
+    const grid = (() => {
+        const arr = _createArray(3, 3);
+        for (let col = 0; col < 3; col++) {
+            for (let row = 0; row < 3; row++) {
+                arr[col][row] = (() => {
+                    const gridSets = document.querySelectorAll(".grid");
+                    gridSets.forEach(gridSet => {
+                        if (gridSet.dataset.item == (col.toString() + row.toString())) {
+                            gridSet.addEventListener('click', () => {
+                                gridSet.classList.toggle('circle-sign');
+                            });
+                        }
+                    });
+                })();
+                
+            }
+        }
+        return arr;
+    })();
+    
+
+    return {grid};
+})();
