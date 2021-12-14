6e24e05e0dd56f65cbd1547aaa349902bd581209
AlliesChen <cyphuaenng@pm.me>
2021-12-14 12:51:47 +0800

    Add reset button; step 6/7

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Tic-Tac-Toe
 
+[Live preview](https://allieschen.github.io/tic-tac-toe/)
+
+[How the system work(flowchart)](https://drive.google.com/file/d/156GqDK-x4kFQ9bUXjeG0POJVVMCNRJtR/view?usp=sharing)
+
+## Requirements
+
+- [x] Starting
+  - Store the gameboard as an array inside of a Gameboard object.
+  - Players are also going to be stored in objects.
+  - An object to control the flow of the game itself.
+- [x] Set up your HTML and write a JavaScript function that will render the contents of the gameboard array to the webpage.
+- [x] Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, letting players click on the gameboard to place their marker.
+- [x] Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
+- [x] Reset the game
+  - Clean up the interface to allow players to put in their names
+  - Include a button to start/restart the game
+  - Add a display element that congratulates the winning player!
+- [ ] Optional: create an AI so that a player can play against the computer! 
+  - Start by just getting the computer to make a random legal move.
+  - Create an unbeatable AI using the minimax algorithm
+
+For the step 2 & 3, I didn't follow the requirement as I don't want the array, which stores the game board to be touched (publicly).
+
 ## Reference
 
 The way to built a two dimentional array is refered to [this discussion](https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938). And an [explain](https://stackoverflow.com/questions/960866/how-can-i-convert-the-arguments-object-to-an-array-in-javascript) for why it can turn `arguments` object to an array, also can be find in [MDN Docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments)
@@ -8,3 +31,5 @@ Additionally, for the detail of methods, which are also the key to implement the
 
 - [Function.prototype.call()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call)
 - [Funciton.prototype.apply()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply)
+
+To remove the event listener, refer to [the comment](https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript) by Adam Heath.
--- js-file.js
+++ js-file.js
@@ -1,89 +1,157 @@
-const Grid = {
-    get list() {
-        return document.querySelectorAll(".grid");
+const Player = (name) => {
+    function playerInput() {
+        return (document.getElementById(name).value || name);
     }
+    return {playerInput}
 };
 
-const gameBoard = (() => {
-    // create a two dimentional array
+const GameBoard = (() => {
+    // assign two dimentional array
+    const _gridArr = _createArray(3, 3);
+    const announceBlock = document.getElementById("announceBlock");
+    const announceResult = document.getElementById("announceResult")
+    const announceName = document.getElementById("announceName")
+
+    // clean the array value
+    const gameResetBtn = document.getElementById("gameResetBtn");
+    gameResetBtn.addEventListener("click", () => {
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                _gridArr[row][col] = "";
+            }
+        }
+        DisplayController.resetGame();
+        announceBlock.classList.add("blank");
+    });
+
+    // functions
+
     function _createArray(length) {
         const arr = new Array(length || 0);
         let i = length;
-    
+        
         if (arguments.length > 1) {
             const args = Array.prototype.slice.call(arguments, 1);
             while(i--) arr[length-1 - i] = _createArray.apply(this, args);
         }
         return arr;
     };
-
-    const grids = (() => {
-        const gridArr = _createArray(3, 3);
-        return gridArr;
-    })();
-
-    // to controll the game flow base on rounds
-    const _displayController = (() => {
-        let _round = 1;
-        function setRound() {
-            let currentRound = _round;
-            _round++;
-            return currentRound;
-        }
-        function checkGame() {
-            if (_round > 9) {
-                for(let col of grids) {
-                    for(let row of col) {
-                        row = "";
-                    }
-                }
-                _round = 1;
-                alert("Game Over")
-            } else {
-                return false;
-            }
-            return true;
-        }
-        function resetGame() {
-            Grid.list.forEach(grid => {
-                grid.classList.remove("cross-sign");
-                grid.classList.remove("circle-sign");
-            });
-            return 0;
-        }
-        return {setRound, checkGame, resetGame};
-    })();
-
-    function checkAction(dataItem, playerName) {
-        const dataArr = dataItem.split("");
-        Grid.list.forEach(grid => {
-            gridClass = grid.getAttribute("class");
-            if(dataItem === grid.dataset.item && !gridClass.match(/cross-sign|circle-sign/)) {
-                if(_displayController.setRound() % 2) {
-                    grid.classList.add("cross-sign");
-                    grids[dataArr[0]][dataArr[1]] = "x"
-                } else {
-                    grid.classList.add("circle-sign");
-                    grids[dataArr[0]][dataArr[1]] = "o"
-                }
-                console.log(playerName + " draw the grid " + dataItem);
-            }
-        });
-        _displayController.checkGame();
+    
+    function recordRound(round) {
+        const gridItem = Array.from(round.getGridItem());
+        _gridArr[gridItem[0]][gridItem[1]] = round.getPlayer().sign;
+        _checkGame(round.getPlayer());
     };
 
-    return {checkAction};
+    function _checkGame(player) {
+        for (let row = 0; row < 3; row++) {
+            for (let pattern = 0; pattern < 4; pattern++) {
+                let count = 0;
+                for (let col = 2; col >= 0; col--) {
+                    switch (pattern) {
+                        case 0:
+                            if (_gridArr[row][col] === player.sign) {
+                                count++;
+                            }
+                            break;
+                        case 1:
+                            if (_gridArr[col][row] === player.sign) {
+                                count++;
+                            }
+                            break;
+                        case 2:
+                            if (_gridArr[col][col] === player.sign) {
+                                count++;
+                            }
+                            break;
+                        default:
+                            if (_gridArr[2 - col][col] === player.sign) {
+                                count++;
+                            }
+                    }
+                }
+                if (count === 3) {
+                    announceBlock.classList.remove("blank");
+                    announceResult.textContent = "Winner"
+                    announceName.textContent = player.playerInput();
+                    roundInfo.classList.add("blank");
+                    return 0;
+                }
+            }
+        }
+        if(DisplayController.checkGame()) {
+            announceBlock.classList.remove("blank");
+            announceResult.textContent = "Tie!"
+            roundInfo.classList.add("blank");
+        }
+        return 0; 
+    }
+    
+    return {recordRound};
 })();
 
+// to controll the game flow base on rounds
+const DisplayController = (() => {
+    let _round = 0;
+    const _grids =  document.querySelectorAll(".grid");
+    const gameStartBtn = document.getElementById("gameStartBtn");
+    const playStart = document.getElementById("playStart");
+    const roundInfo = document.getElementById("roundInfo");
+    const playerName = document.getElementById("playerName");
+    const player1 = Object.assign(Player("player1"), {sign: "x"});
+    const player2 = Object.assign(Player("player2"), {sign: "o"});
 
-const player = {
-    markGrid(name) {
-        Grid.list.forEach(grid => {
-            grid.addEventListener('click', () => {
-                gameBoard.checkAction(grid.dataset.item, name);
-            });
+    const handler = function(e) {
+        GameBoard.recordRound(_roundCheck(e.target));
+    };
+
+    gameStartBtn.addEventListener("click", () => {
+        _grids.forEach(grid => {
+            grid.addEventListener("click", handler);
         });
-    },
-};
+        playerName.textContent = player1.playerInput();
+        playStart.classList.add("blank");
+        roundInfo.classList.remove("blank");
+    });
 
-const player1 = player.markGrid("player1");
+    const _roundCheck = (grid) => {
+        if (!grid.className.match(/cross-sign|circle-sign/)) {
+            _round++;
+            (_round % 2) ? grid.classList.add("cross-sign"): grid.classList.add("circle-sign");
+        }
+        
+        const getGridItem = () => grid.dataset.item;
+        const getPlayer = () => {
+            if (_round % 2) {
+                playerName.textContent = player2.playerInput();
+                return player1;
+            } else {
+                playerName.textContent = player1.playerInput();
+                return player2;
+            }
+        };
+        return {getGridItem, getPlayer};
+    };
+
+    function checkGame() {
+        if (_round > 8) {
+            _round = 1;
+            return true;
+        } else {
+            return false
+        }
+    }
+
+    function resetGame() {
+        _grids.forEach(grid => {
+            grid.classList.remove("cross-sign");
+            grid.classList.remove("circle-sign");
+            grid.removeEventListener("click", handler);
+        });
+        _round = 0;
+        playStart.classList.remove("blank");
+        return 0
+    }
+    
+    return {checkGame, resetGame};
+})();
